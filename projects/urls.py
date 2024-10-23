0b74b1dcd0926e450ff208eec5d2ad72c052b2c4
junaidalam2 <junaid.alam2@gmail.com>
2024-10-22 23:00:58 -0700

    finished stlying for project detail

--- projects/urls.py
+++ projects/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('<int:pk>/', ProjectDetailView.as_view(), name='projects-detail'),
     path('create/', ProjectCreateView.as_view(), name='projects-create'),
     path('update/<int:pk>/', ProjectUpdateView.as_view(), name='projects-update'), 
-    path('delete/<int:project_id>/', ProjectDeleteView.as_view(), name='projects-delete'),
+    path('delete/<int:pk>/', ProjectDeleteView.as_view(), name='projects-delete'),
     path('project/<int:project_id>/upload/', FileUploadCreateView.as_view(), name='upload_file'),
     path('file_upload/delete/<int:pk>/', FileUploadDeleteView.as_view(), name='file_upload_delete'),
 ]
