1683be00d2cfe23d49e6de534a7077f5ffff76b5
junaidalam2 <junaid.alam2@gmail.com>
2024-10-26 16:59:21 -0700

    added vercel config files

--- basecamp/local_settings.py
+++ basecamp/local_settings.py
@@ -1,3 +1,3 @@
 LOCAL_SETTINGS_SECRET_KEY = 'django-insecure-a6$7gmmlo1eep*(_)w#&3*m0eiryd4(u$srsl&fe@5s6-i(m)t'
-SESSION_COOKIE_SECURE = False  # Set to True in production with HTTPS
+SESSION_COOKIE_SECURE = True  # Set to True in production with HTTPS
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
