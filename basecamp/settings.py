cdc11b82b42e9c81659ed18939b962a7be205ddf
junaidalam2 <junaid.alam2@gmail.com>
2024-10-28 08:18:35 -0700

    updated allowed hosts

--- basecamp/settings.py
+++ basecamp/settings.py
@@ -30,8 +30,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
-#'.vercel.app'
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'basecamp-part2-django.onrender.com',
+]
 
 # Application definition
 
