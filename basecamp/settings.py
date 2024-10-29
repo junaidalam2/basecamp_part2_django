bf631e14664a69c8d92e5de6b864dbc6aa038568
junaidalam2 <junaid.alam2@gmail.com>
2024-10-29 00:13:35 -0700

    updated static directory

--- basecamp/settings.py
+++ basecamp/settings.py
@@ -156,10 +156,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles/images',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
